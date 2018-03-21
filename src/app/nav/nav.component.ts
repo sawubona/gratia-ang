ba3012efe7178c0dc76813c609a1185a4c7e1b89
sawubona <sawubona36@gmail.com>
2018-03-21 13:32:46 +0300

    Rebuild nav, slider

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
 })
 export class NavComponent {
 
+  links = [{ link: '#', name: 'About', id: 'about' },
+  { link: '#', name: 'Menu', id: 'menu' },
+  { link: '#', name: 'Gallery', id: 'gallery' },
+  { link: '#', name: 'Contact', id: 'contact' }];
+
   constructor() { }
 
   
--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -5,8 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.css']
 })
+
+
 export class SliderComponent {
 
+  slides = [{ name: 'First slide', img_link: '/assets/brand/knife.png' },
+  { name: 'Second slide', img_link: '/assets/brand/knife.png' },
+  { name: 'Third slide', img_link: '/assets/brand/knife.png' },
+  ];
+
   constructor() { }
+
+  setActiveSlide(i: number) {
+    if (i === 0) {
+      return ' active';
+    } else {
+      return '';
+    }
+  }
 
 }
